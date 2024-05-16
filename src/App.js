72c013f4a244256d9addeb402b89a3bcbf90a732
lovekesh-jindal <lovekeshjindal05@gmail.com>
2024-05-17 03:02:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [images,setImages] = useState([]);
+  const [idx,setIdx] = useState(0);
+  const fetchImages = async () => {
+ const url = 'https://www.reddit.com/r/aww/top/.json?t=all';
+ const res = await fetch(url);
+ const result = await res.json();
+ const data = result.data.children;
+ const list = data.filter((item) => {
+  return item.data.url_overridden_by_dest.includes('.jpg')
+  }).map((item) => {
+   return item.data.url_overridden_by_dest
+ });
+ setImages(list);
+  }
+
+  useEffect(()=>{
+   fetchImages();
+  },[])
+  const handleClick = (dir) =>{
+  const lastIndex = images.length - 1;
+  if(dir === 'left'){
+    if(idx === 0 ){
+      setIdx(lastIndex);
+    }else{
+      setIdx((idx) => idx - 1);
+    }
+  }else if(dir === 'right'){
+    if(idx === lastIndex){
+      setIdx(0);
+    }else{
+      setIdx((idx) => idx + 1);
+    }
+  }
+
+  }
+  useEffect(()=>{
+    const tid = setInterval(()=>{
+     handleClick('right');
+    },1000);
+    return () =>{clearInterval(tid)};
+
+  },[idx])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={()=>handleClick('left')}>
+      {"<"}
+    </button>
+    <img src = {images[idx]} alt = 'not found' />
+    <button className='right' onClick={()=>handleClick('left')}>
+      {">"}
+    </button>
     </div>
   );
 }
